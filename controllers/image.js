3889e481335a53f9ec4012f5a62748f0a9602f4f
Badr Eddine Moufid <45129154+BadrEddineMoufid@users.noreply.github.com>
2019-03-11 20:27:53 +0100

    Adding API of the smart brain project

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+
+const app = new Clarifai.App({
+    apiKey: '33130c9249ed4a298014438a4e062579'
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API '))
+}
+
+
+const handleImage = (req, res, db) =>{
+    const { id } = req.body;
+    //console.log({id});
+    db('users').where('id', '=', id)
+    .increment('entries',1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries !! '))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
