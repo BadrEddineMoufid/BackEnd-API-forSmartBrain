79ea9655b613bb959a6b0e6b8435e102aa66675f
Badr Eddine <bin.benh@gmail.com>
2020-07-04 19:52:20 +0100

    added some comments

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-
+//setting up the knex cnx to DB
 const db = knex({
     client: 'pg',
     version: '7.2',
@@ -24,29 +24,31 @@ const db = knex({
 
 const app = express();
 
+//using bodyparser for parsing json
 app.use(bodyParser.json());
 app.use(cors());
 
-
+//root endpoint just for testing if the server is up 
 app.get('/', (req, res) => {
     res.send('if this shows up then then app is working hhhh');
 })
 
+//sign in route
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-
+//register route
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
-
+//profile route--- by user id
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 
-
+//image related routes
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
 
-
+//listenning for req
 app.listen(process.env.PORT || 3000, ()=>{
     console.log(`app is running on Port ${process.env.PORT}`);
 })
